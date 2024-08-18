466cb0903240a3bf594eee79db6f10004b89a192
Ayush-0-7 <22052720@kiit.ac.in>
2024-08-18 20:14:12 +0530

    8th commit

--- api/main.py
+++ api/main.py
@@ -1,26 +1,24 @@
 from flask import Flask, request, jsonify
 import google.generativeai as genai
 from config import *
-
 app = Flask(__name__)
 
 # Set up the API key for Google Generative AI
-genai.configure(api_key='AIzaSyDZwBpgfvc4rMwbEz0Jhkq6B5ADnJpaJM8')
+genai.configure(api_key=api_keys)
 
 @app.route('/genai2', methods=['POST'])
 def genai2():
     # Extract the prompt from the incoming POST request
-    prompt = request.json.get('question1') + ' Hey, from the given text, extract the product name, product price, product discount, and product link in JSON format and return the JSON format only.'
+    prompt = request.json.get('question1')+'Hey from the given text extract the product name product price product discount and product link in json format and return the json format only.'
     
     if not prompt:
         return jsonify({"error": "No prompt provided"}), 400
 
     # Generate content using the Generative AI model
-    response = genai.generate_text(model='gemini-1.5-flash', prompt=prompt)
+    model = genai.GenerativeModel(model_name="gemini-1.5-flash")
+    response = model.generate_content(prompt)
 
-    # Extract and return the generated text response
-    generated_text = response['text']
+    # Extract and return the text response
+    generated_text = response.text
     return jsonify({"answer": generated_text})
 
-if __name__ == "__main__":
-    app.run(debug=True)
