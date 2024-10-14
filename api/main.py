1868aadc3e45360ce3523a5a764191feb433bfe6
Ayush-0-7 <156231875+Ayush-0-7@users.noreply.github.com>
2024-10-14 22:56:41 +0530

    Update main.py

--- api/main.py
+++ api/main.py
@@ -9,7 +9,7 @@ genai.configure(api_key='AIzaSyDZwBpgfvc4rMwbEz0Jhkq6B5ADnJpaJM8')
 @app.route('/genai2', methods=['POST'])
 def genai2():
     # Extract the prompt from the incoming POST request
-    prompt = request.json.get('question1')+'Hey from the given text extract the product name product price product discount and product link in json format and return the json format only.'
+    prompt = request.json.get('question1')+'Hey from the given text extract the product name product price product discount product link and product image in json format and return the json format only.'
     
     if not prompt:
         return jsonify({"error": "No prompt provided"}), 400
